d7b558a9db439d9d2894ab10484e8f90dee80fdb
Drzerald <48493271+Drzerald@users.noreply.github.com>
2020-03-31 20:35:36 +0200

    Creating VisualStudio project files
    Project using:
    .Net Framework 4.7.1,
    WPF window subsystem.

---
+++ WHFR-ed2-NPC-Creator/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WHFR_ed2_NPC_Creator {
+	/// <summary>
+	/// Logika interakcji dla klasy App.xaml
+	/// </summary>
+	public partial class App : Application {
+	}
+}
