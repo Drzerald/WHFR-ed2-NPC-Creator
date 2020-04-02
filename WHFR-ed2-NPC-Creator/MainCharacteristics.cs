318bd56d4c6dee7ac0267e187d0b74f06617abb2
Drzerald <48493271+Drzerald@users.noreply.github.com>
2020-04-03 01:22:13 +0200

    Implement Characteristics

---
+++ WHFR-ed2-NPC-Creator/MainCharacteristics.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WHFR_ed2_NPC_Creator
+{
+    class MainCharacteristics
+    {
+		public int weaponSkills;
+		public int baliscticSkills;
+		public int strength;
+		public int toughness;
+		public int agility;
+		public int intelligence;
+		public int willPower;
+		public int fellowship;
+	}
+
+}
