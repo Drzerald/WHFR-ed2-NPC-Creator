9183ab6d199d996d2c9375ac80bcf2d991cb74b8
nathanaee1 <61773602+nathanaee1@users.noreply.github.com>
2020-05-11 23:25:04 +0200

    Update Character.cs

--- WHFR-ed2-NPC-Creator/Character.cs
+++ WHFR-ed2-NPC-Creator/Character.cs
@@ -10,6 +10,9 @@ namespace WHFR_ed2_NPC_Creator {
 		public string name = "";
 
 		
+
+
+
 		public Characteristics characteristicsFromRace = new Characteristics();
 		public Characteristics characteristicsFromRolls = new Characteristics();
 		public Characteristics characteristicsFromProffessions = new Characteristics();
@@ -18,9 +21,39 @@ namespace WHFR_ed2_NPC_Creator {
 
 		Profession[] professionHistory = new Profession[3];
 
-		public SkillList skills= new SkillList();
+		public SkillList skills = new SkillList();
 		public List<Talent> talents = new List<Talent>();
-		//public TalentList talens = new TalentList(); //Idea, Is Talent List class is importanee
 
+		public Character() {
+			rerollCharateristics();
+		}
+
+		public void rerollCharateristics() {
+			DieRoller die = new DieRoller();
+			characteristicsFromRolls.WeaponSkills = die.rollD10(2);
+			characteristicsFromRolls.BaliscticSkills = die.rollD10(2);
+			characteristicsFromRolls.Strength = die.rollD10(2);
+			characteristicsFromRolls.Toughness = die.rollD10(2);
+			characteristicsFromRolls.Agility = die.rollD10(2);
+			characteristicsFromRolls.Intelligence = die.rollD10(2);
+			characteristicsFromRolls.WillPower = die.rollD10(2);
+			characteristicsFromRolls.Fellowship = die.rollD10(2);
+		}
+
+		public void debugPrint() {
+			System.Diagnostics.Debug.Print(
+				characteristicsFromRolls.WeaponSkills.ToString() + " " +
+				characteristicsFromRolls.BaliscticSkills.ToString() + " " +
+				characteristicsFromRolls.Strength.ToString() + " " +
+				characteristicsFromRolls.Toughness.ToString() + " " +
+				characteristicsFromRolls.Agility.ToString() + " " +
+				characteristicsFromRolls.Intelligence.ToString() + " " +
+				characteristicsFromRolls.WillPower.ToString() + " " +
+				characteristicsFromRolls.Fellowship.ToString()+"\n"
+			);
+			foreach(Skill skill in skills.skillsArray) {
+				System.Diagnostics.Debug.Print(skill.ToString());
+			}
+		}
 	}
 }
